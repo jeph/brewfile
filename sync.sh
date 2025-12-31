39283cfc811076a4997c30c23c2a108398d14d80
jeph <23463082+jeph@users.noreply.github.com>
2025-12-31 02:10:54 -0500

    Remove sync.sh as it does not work

--- sync.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-brewfile_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
-exec brew bundle cleanup --force --zap --file "${brewfile_dir}/Brewfile"
